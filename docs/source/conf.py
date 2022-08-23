cb3a0d9638280615b8473dbb9ce4440c66e15c9d
Galen Seilis <galen@orcus.localdomain>
2022-08-22 20:10:02 -0700

    Fixed static doc path warning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Galen Seilis'
 author = 'Galen Seilis'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.26'
+release = '0.0.34'
 
 
 # -- General configuration ---------------------------------------------------
@@ -55,4 +55,4 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
