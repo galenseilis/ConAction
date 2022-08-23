fac69e4b2eda207bb3605a69f7d3eb5b682d5212
galen <galen.seilis@seilis.ca>
2022-08-23 07:57:36 -0700

    added readthedocs config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Galen Seilis'
 author = 'Galen Seilis'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.40'
+release = '0.0.41'
 
 
 # -- General configuration ---------------------------------------------------
