cf1d75c49019386ee83a2a1befcf0d2747fb24b8
galen <galen.seilis@seilis.ca>
2022-08-24 07:41:39 -0700

    Exclude conaction is path to source in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/conaction'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 
 # -- Project information -----------------------------------------------------
