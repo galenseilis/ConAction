d7f17fd8b60a954178805cb3f6ca95c12ee046c4
galen <galen.seilis@seilis.ca>
2022-08-27 10:51:44 -0700

    Removed update shell script

--- update.sh
+++
@@ -1,9 +0,0 @@
- # Python Packages
-pip install --upgrade pip
-pip install --upgrade build
-pip install --upgrade twine
-pip install --upgrade bump2version
-pip install --upgrade twine
-
-# Current Package
-python3 -m twine upload dist/*
